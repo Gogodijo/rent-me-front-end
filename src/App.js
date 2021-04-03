152542d615982ef3e59c1c15015b865fba41dd4c
Joni Vilokki <36036801+Gogodijo@users.noreply.github.com>
2021-04-03 14:48:07 +0300

    teset backend connection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
+  const [ping, setPing] = useState("")
+  useEffect(  () => {
+    async function pingServer(){
+     const pong = await axios.get("/ping")
+     console.log(pong)
+     setPing(pong.data.msg)
+    }
+    pingServer()
+  }, [])
   return (
     <div>
       Hello World!
       Buildin testausta
+      ping? {ping}
     </div>
   );
 }
